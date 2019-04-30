30aad8c7da3bdc9f66c66d75c1b265a9845f8edd
Sergio Municio <sergio.municio@soprasteria.com>
2019-04-30 15:47:50 +0200

    20

--- 1-jsx/src/index.js
+++ 1-jsx/src/index.js
@@ -2,13 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'Click Me!';
+}
+
 // Create a react component
 const App = () => {
     return (
         <div>
             <label className="label" for="name">Enter name</label>
             <input id="name" type="text"/>
-            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{ getButtonText() }</button>
         </div>
     );
 }
