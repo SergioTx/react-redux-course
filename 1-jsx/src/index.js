708fa593e3a9237b62cda79acbed401e3a85db56
Sergio Municio <sergio.municio@soprasteria.com>
2019-04-30 15:50:21 +0200

    refactor: 2 spaces instead of 4

--- 1-jsx/src/index.js
+++ 1-jsx/src/index.js
@@ -3,22 +3,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function getButtonText() {
-    return 'Click Me!';
+  return 'Click Me!';
 }
 
 // Create a react component
 const App = () => {
-    return (
-        <div>
-            <label className="label" for="name">Enter name</label>
-            <input id="name" type="text"/>
-            <button style={{backgroundColor: 'blue', color: 'white'}}>{ getButtonText() }</button>
-        </div>
-    );
+  return (
+    <div>
+        <label className="label" for="name">Enter name</label>
+        <input id="name" type="text"/>
+        <button style={{backgroundColor: 'blue', color: 'white'}}>{ getButtonText() }</button>
+    </div>
+  );
 }
 
 // Take the react component and show it on the screen
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+  <App />,
+  document.querySelector('#root')
 );
