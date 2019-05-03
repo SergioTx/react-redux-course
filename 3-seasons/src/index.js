625af273245b5ef97437feb260ce9aef13a01506
Sergio Municio <sergio.municio@soprasteria.com>
2019-05-03 12:46:54 +0200

    54

--- 3-seasons/src/index.js
+++ 3-seasons/src/index.js
@@ -5,16 +5,22 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    // the only time we can make direct assignment to state
     this.state = {lat: null};
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // the only way to update state is setState()
+        // cannot do this.state.lat = xxx;
+        this.setState({lat: position.coords.latitude});
+      },
+      (err) => console.log(err)
+    );
   }
 
   // React says we have to define render!!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-    return <div>Latitude: </div>;
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
