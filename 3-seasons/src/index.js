8f0d8a3431d0d664506bbb15e0febf34618f35ba
Sergio Municio <sergio.municio@soprasteria.com>
2019-05-03 14:26:32 +0200

    70

--- 3-seasons/src/index.js
+++ 3-seasons/src/index.js
@@ -13,8 +13,7 @@ class App extends React.Component {
     );
   }
 
-  // React says we have to define render!!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -22,6 +21,15 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+
+  // React says we have to define render!!
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
   }
 }
 
