fb447554be662e4863796e8827c7e9af76f8e653
SergioTx <txitxo37@gmail.com>
2019-05-06 16:04:19 +0200

    115

--- 5-videos/src/apis/youtube.js
+++ 5-videos/src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import YOUTUBE_APIKEY from './config';
+import { YOUTUBE_APIKEY } from './config';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
