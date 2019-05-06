097994f7f6848f35f792e159b568464ab4a4a65d
SergioTx <txitxo37@gmail.com>
2019-05-06 13:18:08 +0200

    92

--- 4-pics/src/components/App.js
+++ 4-pics/src/components/App.js
@@ -5,6 +5,8 @@ import { UNSPLASH_APIKEY } from '../config';
 
 
 class App extends React.Component {
+  state = { images: [] };
+
   async onSearchSubmit(term) {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {
@@ -15,13 +17,14 @@ class App extends React.Component {
         },
     });
 
-    console.log(response.data.results);
+    this.setState({ images: response.data.results});
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={ this.onSearchSubmit } />
+        Found: {this.state.images.length} images
       </div>
     );
   }
