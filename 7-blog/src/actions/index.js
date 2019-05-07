509632eb209160dea6fb3d354b4165fcf1ef12a0
SergioTx <txitxo37@gmail.com>
2019-05-07 14:50:32 +0200

    160

--- 7-blog/src/actions/index.js
+++ 7-blog/src/actions/index.js
@@ -1,5 +1,11 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+  // BAD APPROACH!!! Use middleware
+  const response = await jsonPlaceholder.get('/posts');
+
   return {
     type: 'FETCH_POSTS',
+    payload: response,
   };
 };
