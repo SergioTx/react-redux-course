6eac9851d87db8f8d4c083abeb10065697997a12
SergioTx <txitxo37@gmail.com>
2019-05-07 13:10:49 +0200

    data changes

--- 6-songs/src/reducers/index.js
+++ 6-songs/src/reducers/index.js
@@ -2,10 +2,10 @@ import { combineReducers } from 'redux';
 
 const songsReducer = () => {
   return [
-    { title: 'No Scrubs', duration: '4:05' },
-    { title: 'Macarena', duration: '2:30' },
-    { title: 'All Star', duration: '3:15' },
-    { title: 'I want it that way', duration: '1:45' },
+    { title: 'Nevermind', duration: '4:05' },
+    { title: 'Hells Bells', duration: '2:30' },
+    { title: 'The Trooper', duration: '3:15' },
+    { title: 'Holy Diver', duration: '1:45' },
   ];
 };
 
