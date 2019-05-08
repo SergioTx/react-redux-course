dfa34fffd68d9f181d239ca58ac3238bd655146b
SergioTx <txitxo37@gmail.com>
2019-05-08 16:20:37 +0200

    188

--- 7-blog/src/actions/index.js
+++ 7-blog/src/actions/index.js
@@ -2,10 +2,10 @@ import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPostsAndUsers = () =>
-  async (dispatch) => {
-    console.log('about to fetch posts');
+  async (dispatch, getState) => {
     await dispatch(fetchPosts());
-    console.log('fetchedposts');
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach((id) => dispatch(fetchUser(id))); // no need to wait for all users
   }
 
 export const fetchPosts = () =>
