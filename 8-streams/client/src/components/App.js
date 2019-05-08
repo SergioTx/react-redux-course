22055ad3e2b36298db2cdbc1c429c9d692db2bc9
SergioTx <txitxo37@gmail.com>
2019-05-08 17:09:05 +0200

    195

--- 8-streams/client/src/components/App.js
+++ 8-streams/client/src/components/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Page1 = () => {
+  return <div>Page1</div>
+}
+const Page2 = () => {
+  return <div>Page2
+    <button>Click me</button>
+  </div>
+}
+
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={Page1} />
+          <Route path="/pagetwo" component={Page2} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
