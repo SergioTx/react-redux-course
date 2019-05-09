ac1159bb577bec90afeaa1c6192a546022e1c837
SergioTx <txitxo37@gmail.com>
2019-05-09 13:22:44 +0200

    211

--- 8-streams/client/src/components/GoogleAuth.js
+++ 8-streams/client/src/components/GoogleAuth.js
@@ -2,17 +2,34 @@ import React from 'react';
 import { GAPI_TOKEN } from '../config';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: GAPI_TOKEN,
         scope: 'email',
-      });
+      })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
+  renderAuthButton() {
+    debugger;
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>
+    } else {
+      return <div>I am not signed in</div>
+    }
+  }
+
   render() {
-    return <div>GoogleAuth</div>
+    return <div>{this.renderAuthButton()}</div>
   }
 }
 
