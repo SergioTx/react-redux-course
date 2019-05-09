f148bc76211c399c720179a03ae3daa670edb7ac
SergioTx <txitxo37@gmail.com>
2019-05-09 16:42:20 +0200

    229

--- 8-streams/client/src/components/streams/StreamCreate.js
+++ 8-streams/client/src/components/streams/StreamCreate.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
   render() {
     return (
       <form>
-        <Field name="title" />
-        <Field name="description" />
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
       </form>
     );
   }
 };
 
 export default reduxForm({
-  form: '',
+  form: 'streamCreate',
 })(StreamCreate);
