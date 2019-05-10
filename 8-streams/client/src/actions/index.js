78a549aff14dd3d3b578f37f0d75c11a470c842d
SergioTx <txitxo37@gmail.com>
2019-05-10 14:02:16 +0200

    253

--- 8-streams/client/src/actions/index.js
+++ 8-streams/client/src/actions/index.js
@@ -29,6 +29,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
     type: CREATE_STREAM,
     payload: response.data,
   });
+  // Do some programmatic navigation back to the root route
 };
 
 export const fetchStreams = () => async (dispatch) => {
