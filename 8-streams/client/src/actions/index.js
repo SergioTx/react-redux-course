0c167e4082f0de61769bd7ce535f8c846249d124
SergioTx <txitxo37@gmail.com>
2019-05-10 14:13:50 +0200

    257

--- 8-streams/client/src/actions/index.js
+++ 8-streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -30,6 +31,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
     payload: response.data,
   });
   // Do some programmatic navigation back to the root route
+  history.push('/');
 };
 
 export const fetchStreams = () => async (dispatch) => {
