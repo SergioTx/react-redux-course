df19eec1f9344b0c8711495b5afa31aceb56b543
SergioTx <txitxo37@gmail.com>
2019-05-13 14:31:00 +0200

    292

--- 8-streams/client/src/components/streams/StreamShow.js
+++ 8-streams/client/src/components/streams/StreamShow.js
@@ -9,10 +9,23 @@ class StreamShow extends React.Component {
 
     this.videoRef = React.createRef();
   }
+
   componentDidMount() {
     const { id } = this.props.match.params;
 
-    this.props.fetchStream(this.props.match.params.id);
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) return;
+
+    const { id } = this.props.match.params;
+
     this.player = flv.createPlayer({
       type: 'flv',
       url: `http://localhost:8000/live/${id}.flv`,
@@ -20,6 +33,7 @@ class StreamShow extends React.Component {
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
   }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
