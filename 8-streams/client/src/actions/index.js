22d475d8c7006b077fea3b6369569e20e33976fc
SergioTx <txitxo37@gmail.com>
2019-05-13 12:16:57 +0200

    281

--- 8-streams/client/src/actions/index.js
+++ 8-streams/client/src/actions/index.js
@@ -66,4 +66,6 @@ export const deleteStream = (id) => async (dispatch) => {
     type: DELETE_STREAM,
     payload: id,
   });
+  // Do some programmatic navigation back to the root route
+  history.push('/');
 };
