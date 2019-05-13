d460865e6370c5c0027dfde375295d87f91e7f70
SergioTx <txitxo37@gmail.com>
2019-05-13 16:03:14 +0200

    307

---
+++ 9-translate/src/components/LanguageSelector.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Field from './Field';
+import Button from './Button';
+
+class LanguageSelector extends React.Component {
+  render() {
+    return (
+      <div>
+        Select a language:
+          <i className="flag us" onClick={() => this.props.onLanguageChange('english')} />
+        <i className="flag nl" onClick={() => this.props.onLanguageChange('dutch')} />
+      </div>
+    );
+  }
+};
+
+export default LanguageSelector;
