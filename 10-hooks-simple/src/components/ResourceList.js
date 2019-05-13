5a82a3784d737472a10e27cf3f654259c1d528a7
SergioTx <txitxo37@gmail.com>
2019-05-13 17:05:29 +0200

    321

--- 10-hooks-simple/src/components/ResourceList.js
+++ 10-hooks-simple/src/components/ResourceList.js
@@ -2,11 +2,18 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 class ResourceList extends React.Component {
-  state = { resources: [] }
+  state = { resources: [] };
 
   async componentDidMount() {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
     this.setState({ resources: response.data });
+  }
+
+  async componentDidUpdate(prevProps) {
+    if (prevProps !== this.props.resource) {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+      this.setState({ resources: response.data });
+    }
   }
 
   render() {
