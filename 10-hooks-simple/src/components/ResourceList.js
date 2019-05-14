6e8492499eb07c0b6fdf4e71425fccd4e3a85aa7
SergioTx <txitxo37@gmail.com>
2019-05-14 09:39:31 +0200

    326

--- 10-hooks-simple/src/components/ResourceList.js
+++ 10-hooks-simple/src/components/ResourceList.js
@@ -4,13 +4,11 @@ import axios from 'axios';
 const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async (resource) => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-    setResources(response.data);
-  };
-
   useEffect(() => {
-    fetchResource(resource);
+    (async (resource) => {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+      setResources(response.data);
+    })(resource);
   }, [resource]); //second argument controls if the function should be invoked
 
   return (
